39b697e1939f0f9b1c506ac037e1306ff83a6693
JALINDAR01 <jalindar10yewale@gmail.com>
2023-12-29 22:35:14 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../component/Common/Header'
+import MainComponent from '../component/LandingPage'
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+        <MainComponent />
+
+    </div>
+  )
+}
+
+export default Home
